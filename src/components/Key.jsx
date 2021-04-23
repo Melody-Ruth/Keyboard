c3b8a3edcd8f2885ef392288db4468c254bc8adb
Melody-Ruth <melody@sd-fun.org>
2021-04-23 07:57:47 -0700

    fixing spacebar

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -6,7 +6,7 @@ export default function Keyboard(props) {
         <div>
             {
                 props.letter == "space" ?
-                <button className='key keySpace' onClick={() => {props.changeText(" ")}}></button>
+                <button className='key keySpace' onClick={() => {props.changeText("\xa0")}}></button>
                 :
                 <button className='key' onClick={() => {props.changeText(props.letter)}}>{props.letter}</button>
             }
